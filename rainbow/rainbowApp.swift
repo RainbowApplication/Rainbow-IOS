b62a1d36ad298b5048198d4f0c32766308e190f2
GoldenJayz <45317852+GoldenJayz@users.noreply.github.com>
2022-09-16 15:12:00 -0700

    Initial Commit

---
+++ rainbow/rainbowApp.swift
@@ -0,0 +1,17 @@
+//
+//  rainbowApp.swift
+//  rainbow
+//
+//  Created by Jaden D on 9/16/22.
+//
+
+import SwiftUI
+
+@main
+struct rainbowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
